dae80f671e88a52e9c79b5e1227098b7f8f7090b
nkinnacostly <ayangcostly@gmail.com>
2024-09-20 00:46:27 +0100

    feat:fixes on the homepage

--- middleware.js
+++ middleware.js
@@ -3,17 +3,17 @@
 import { NextResponse } from "next/server";
 
 export default function middleware(req) {
-  // console.log(req);
-  // let loggedin = req.cookies.get("__session");
-  // const { pathname } = req.nextUrl;
-  // if (loggedin && pathname === "/login") {
-  //   return NextResponse.redirect(new URL("/dashboard", "127.0.0.1:3000"));
-  // }
-  // if (!loggedin && pathname.includes("/dashboard")) {
-  //   return NextResponse.redirect(new URL("/login", "127.0.0.1:3000"));
-  // }
+  console.log(req);
+  let loggedin = req.cookies.get("__session");
+  const { pathname } = req.nextUrl;
+  if (loggedin && pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard"));
+  }
+  if (!loggedin && pathname.includes("/dashboard")) {
+    return NextResponse.redirect(new URL("/login"));
+  }
 }
 
-// export const config = {
-//   matcher: "/((?!api|static|.*\\..*|_next).*)",
-// };
+export const config = {
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
+};
