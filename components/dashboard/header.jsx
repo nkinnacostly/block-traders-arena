3ccb1e4567d6776ec206a93e90a2fc723fbafddd
Costly Ayang <39498038+nkinnacostly@users.noreply.github.com>
2025-07-29 14:41:38 +0100

    Update header.jsx

--- components/dashboard/header.jsx
+++ components/dashboard/header.jsx
@@ -13,41 +13,6 @@ function DashboardHeader() {
         <div>
           <Image src={Logo} height={100} width={100} alt="logo" />
         </div>
-        <div className="flex items-center space-x-5">
-          <Link href={"/"} className="">
-            Home
-          </Link>
-          <Link href={"/about"} className="">
-            About Us
-          </Link>
-          <Link href={"/education"} className="flex items-center ">
-            <p>Education Arena</p>
-            <span>
-              <Image src={Down} alt="carret-down" height={35} width={35} />
-            </span>
-          </Link>
-          <Link href={"/trading"} className="flex items-center ">
-            <p>Trading Arena</p>
-            <span>
-              <Image src={Down} alt="carret-down" height={35} width={35} />
-            </span>
-          </Link>
-        </div>
-        <div className="flex items-center justify-center space-x-5">
-          {/* <Buttonwithoutbg Btntext={"Sign in"} /> */}
-          {/* <Buttonwithbg btnText={"Create account"} /> */}
-          {/* <div className="w-20 h-20 border-2 rounded-full">
-            <Image
-              src={
-                loggedInUserDetails?.image_url ?? "/assets/img/png/chef.png"
-              }
-              width={100}
-              height={100}
-              className="w-full h-full rounded-full"
-              alt="prof-img"
-            />
-          </div> */}
-        </div>
       </div>
     </div>
   );
